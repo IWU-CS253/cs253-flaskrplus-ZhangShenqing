a5ca247d43b8ebb53b024a3bb409cab0307242b5
ZhangShenqing <jzhang2@iwu.edu>
2025-02-24 03:37:04 -0600

    Step 2 - Ability for user to input category is added.

--- app.py
+++ app.py
@@ -75,8 +75,8 @@ def show_entries():
 @app.route('/add', methods=['POST'])
 def add_entry():
     db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-               [request.form['title'], request.form['text']])
+    db.execute('insert into entries (title, text) values (?, ?， ？)',
+               [request.form['title'], request.form['category'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
     return redirect(url_for('show_entries'))
